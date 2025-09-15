07cd05ce65bb1aed5181914af52827cf22b99271
JunheeLee <80439077+junheelee411@users.noreply.github.com>
2025-09-15 19:41:59 +0900

    Add files via upload

---
+++ IO_Operation/Quiz01.java
@@ -0,0 +1,10 @@
+package IO_Operation;
+
+public class Quiz01 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
---
+++ IO_Operation/Quiz_10172.java
@@ -0,0 +1,16 @@
+package IO_Operation;
+
+public class Quiz_10172 {
+
+	public static void main(String[] args) {
+		// 강아지 출력
+		
+		System.out.println("|\\_/|\r\n"
+				+ "|q p|   /}\r\n"
+				+ "( 0 )\"\"\"\\\r\n"
+				+ "|\"^\"`    |\r\n"
+				+ "||_/=\\\\__|");
+
+	}
+
+}
